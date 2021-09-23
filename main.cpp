d7b1d721ac5151880ec7d0b10e6706dd74c85090
flySanta <a.y.soldatova@sudent.khai.edu>
2021-09-23 14:39:20 +0300

    Add comment2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[ ])
 QApplication app(argc, argv);
  
 QPushButton button("Hello, World!"); //comment
-button.resize(200, 60);
+button.resize(200, 60); //comment222
 button.show( );
  
 return app.exec( );
