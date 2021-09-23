e722f9293a519d3ce39abc96bed0fc85ea19c573
Karat <you@ex.com>
2021-09-23 14:20:56 +0300

    Add comment

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, World!");
+QPushButton button("Hello, World!"); //comment
 button.resize(200, 60);
 button.show( );
  
